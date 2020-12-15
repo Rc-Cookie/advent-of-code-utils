99d9ae4c5b5e46872b5ab945a88b526d3746e499
RcCookie0 <70144256+RcCookie0@users.noreply.github.com>
2020-12-15 11:06:37 +0100

    Improved output of days

--- src/main/java/com/github/rccookie/adventofcode/util/Launcher.java
+++ src/main/java/com/github/rccookie/adventofcode/util/Launcher.java
@@ -66,7 +66,7 @@ public class Launcher {
             if(RUN_ALL) {
                 for(int i=1; i<day; i++) {
                     runDay(i, year);
-                    System.out.println("\n-------------------------------------------------\n-------------------------------------------------\n");
+                    Console.newLine(2);
                 }
             }
             runDay(day, year);
@@ -89,8 +89,10 @@ public class Launcher {
         ctor.setAccessible(true);
         Day dayInstance = ctor.newInstance();
 
+        Console.log("-------------------- Day " + day + " --------------------");
+        Console.newLine();
+
         Console.map("Result of part 1 from day " + dayInstance.getDay(), dayInstance.resultPart1());
-        System.out.println("\n-------------------------------------------------\n");
         try {
             Console.map("Result of part 2 from day " + dayInstance.getDay(), dayInstance.resultPart2());
         } catch(NotImplementedException e) { }
